f5bbdb3e23ae2ecae18b1729990bdd03d28c8f40
winstxnhdw <winstxnhdw@gmail.com>
2022-06-25 06:08:58 +0800

    fix

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
+from json import loads, dumps
 from sys import argv
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 class Server(SimpleHTTPRequestHandler):
 
     def do_POST(self):
+
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length).decode('utf-8')
-        print(f"POST Request:\nPath: {self.path}\nHeaders:\n{self.headers}\n\nBody:\n{post_data}")
+        formatted_data = '\n'.join(line.strip() for line in dumps(loads(post_data), indent=2).lstrip('{').rstrip('}').split('\n'))
+        print(f"\nPOST\n{self.headers}Body:\n{formatted_data}")
         self.send_response(200)
-        self.wfile.write(f"POST request for {self.path.encode('utf-8')}")
 
 def init_server():
 
@@ -17,14 +19,14 @@ def init_server():
     httpd = HTTPServer((host, port), Server)
 
     try:
-        print(f"Serving HTTP on {host} port {port} (http://{host}:{port}/) ...")
+        print(f"Serving HTTP on {host} port {port} (http://{host}:{port}/)..")
         httpd.serve_forever()
 
     except KeyboardInterrupt:
         pass
 
     finally:
-        print("Server closing..")
+        print("\nServer closing..")
         httpd.server_close()
 
 def parse_args():
